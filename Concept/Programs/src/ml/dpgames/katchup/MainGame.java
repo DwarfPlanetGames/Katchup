f556ed988e476e18a0e091e86f61eed4f77a7b9c
BrandonDyer64 <brandondyer64@gmail.com>
2015-09-03 09:01:46 -0400

    Signed-off-by: BrandonDyer64 <brandondyer64@gmail.com>

--- Concept/Programs/src/ml/dpgames/katchup/MainGame.java
+++ Concept/Programs/src/ml/dpgames/katchup/MainGame.java
@@ -3,14 +3,19 @@ package ml.dpgames.katchup;
 import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 
 import javax.swing.JFrame;
 
-public class MainGame implements Runnable {
+public class MainGame implements Runnable, MouseListener, MouseMotionListener {
 
 	// Window
 	private JFrame frame;
 	private Canvas canvas;
+	public static int mouseX = 0, mouseY = 0;
+	public static boolean mouseDown = false;
 	public static int camX = 0, camY = 0;
 
 	// System
@@ -24,13 +29,22 @@ public class MainGame implements Runnable {
 		frame.setSize(640, 400);
 		// Setup canvas
 		canvas = new Canvas();
+		canvas.addMouseListener(this);
+		canvas.addMouseMotionListener(this);
 		frame.add(canvas);
 		// Start game
+		frame.setVisible(true);
+		start();
+	}
+
+	public static void main(String[] args) {
+		new MainGame();
 	}
 
 	public synchronized void start() {
 		if (running)
 			return;
+		running = true;
 		thread = new Thread(this, "Katchup");
 		thread.start();
 	}
@@ -46,7 +60,7 @@ public class MainGame implements Runnable {
 			}
 			render();
 			try {
-				Thread.sleep(1000 / 120);
+				Thread.sleep(15);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
@@ -67,12 +81,49 @@ public class MainGame implements Runnable {
 		g.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
 		g.setColor(Color.WHITE);
 		g.translate(camX, camY);
-		
-		
-		
+
+		if (mouseDown) {
+			g.fillRect(mouseX - 16, mouseY - 16, 32, 32);
+		}
+
 		g.translate(-camX, -camY);
 		g.dispose();
 		canvas.getBufferStrategy().show();
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent arg0) {
+		mouseX = arg0.getX();
+		mouseY = arg0.getY();
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent arg0) {
+		mouseX = arg0.getX();
+		mouseY = arg0.getY();
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		mouseDown = false;
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		mouseDown = true;
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		mouseDown = false;
 	}
 
 }
